de5cc062c2510d9712a2722a38c7842381d21213
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-30 10:08:23 +0530

    Create 72.java

---
+++ 72.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String str="aeiou";
+        int count=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(str.contains(String.valueOf(s.charAt(i))))
+            {
+                count++;
+            }
+        }
+        if(count>0)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
+		
+	
