52ea24107d8dd70163a5ac2bad2739e2e1334aca
naveenramya <41774158+naveenramya@users.noreply.github.com>
2018-08-30 10:30:17 +0530

    Create 73.java

---
+++ 73.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int l=sc.nextInt();
+        int r=sc.nextInt();
+        int count=0;
+        for(int i=l;i<r;i++)
+        {
+            if(n==i)
+            {
+                count++;
+            }
+        }
+        if(count>0)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
+		
+	
